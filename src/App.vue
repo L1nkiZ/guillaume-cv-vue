a6148e91577be05575aed36c2b19659a9120375a
Guillaume G <95032512+L1nkiZ@users.noreply.github.com>
2023-08-15 13:16:44 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <SidebarComponent />
+
+  <RouterView />
+</template>
+
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+  import SidebarComponent from './components/sidebar/SidebarComponent.vue';
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<template>
+  <main>
+    <HomePage />
+  </main>
+</template>
+
+
+<script setup>
+import HomePage from '@/components/home/HomePage.vue'
+</script>
+
